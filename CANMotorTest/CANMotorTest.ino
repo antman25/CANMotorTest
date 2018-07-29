9d7d511dade42140cf10900a3c512ffd333e4f44
Anthony Magner <anthony.magner@gmail.com>
2018-07-28 23:12:47 -0600

    updates

--- CANMotorTest/CANMotorTest.ino
+++ CANMotorTest/CANMotorTest.ino
@@ -1,24 +1,42 @@
 #include "CANTalonSRX.h"
 #include <ros.h>
+#include <titan_base/Status.h>
+#include <titan_base/MotorVelocity.h>
 
 
-#define LEFT_MASTER_ID    1
-#define LEFT_SLAVE_1_ID   2
-#define LEFT_SLAVE_2_ID   3
+#define LEFT_MASTER_ID        1
+#define LEFT_SLAVE_1_ID       2
+#define LEFT_SLAVE_2_ID       3
 
-#define RIGHT_MASTER_ID    4
-#define RIGHT_SLAVE_1_ID   5
-#define RIGHT_SLAVE_2_ID   6
+#define RIGHT_MASTER_ID       4
+#define RIGHT_SLAVE_1_ID      5
+#define RIGHT_SLAVE_2_ID      6
 
-CANTalonSRX motorLeft(LEFT_MASTER_ID,10);
+#define TOTAL_MOTORS          6
+
+#define TIMEOUT_MOTOR_CMD     300
+#define TIMEOUT_MOTOR_STATUS  50
+
+/*CANTalonSRX motorLeft(LEFT_MASTER_ID,10);
 CANTalonSRX motorLeft_Slave1(LEFT_SLAVE_1_ID,10);
 CANTalonSRX motorLeft_Slave2(LEFT_SLAVE_2_ID,10);
 
 CANTalonSRX motorRight(RIGHT_MASTER_ID,10);
 CANTalonSRX motorRight_Slave1(RIGHT_SLAVE_1_ID,10);
-CANTalonSRX motorRight_Slave2(RIGHT_SLAVE_2_ID,10);
+CANTalonSRX motorRight_Slave2(RIGHT_SLAVE_2_ID,10);*/
 
-uint32_t prev_time = millis();
+CANTalonSRX motor[6];
+
+titan_base::Status motor_status;
+titan_base::MotorVelocity vel_cmd;
+
+ros::Publisher pubMotorStatus("motor_status", &motor_status);
+
+
+float left_motor_sp = 0.0f;
+float right_motor_sp = 0.0f;
+
+
 byte debug = 1;
 FlexCAN CANbus0;
 
@@ -26,9 +44,80 @@ float F_gain = 1.0;//10.0 / 1024.0;
 float P_gain = 1.4;
 float I_gain = 0.01;
 float D_gain = 20.0;
-void setup(void)
+
+long timerMotorTimeout = millis();
+long timerMotorStatus = millis();
+
+long seq = 0;
+
+
+void cbMotorVelocity( const titan_base::MotorVelocity &msg)
+{
+  left_motor_sp = (float)msg.left_angular_vel;
+  right_motor_sp = (float)msg.right_angular_vel;
+  timerMotorTimeout = millis();
+}
+
+ros::Subscriber<titan_base::MotorVelocity> subMotorVelocity("motor_velocity", cbMotorVelocity);
+
+void publishMotorStatus()
 {
   
+}
+
+void checkTimers()
+{
+  if (millis() - timerMotorTimeout > TIMEOUT_MOTOR_CMD)
+  {
+    left_motor_sp = 0.0f;
+    right_motor_sp = 0.0f;
+  }
+
+  if (millis() - timerMotorStatus > TIMEOUT_MOTOR_STATUS)
+  {
+   timerMotorStatus = millis();
+   
+   publishMotorStatus();
+  }
+}
+
+void configurePID()
+{
+  motorLeft.SetFgain(0, F_gain);
+  motorLeft_Slave1.SetFgain(0, F_gain);
+  motorLeft_Slave2.SetFgain(0, F_gain);
+  
+  motorRight.SetFgain(0, F_gain);
+  motorRight_Slave1.SetFgain(0, F_gain);
+  motorRight_Slave2.SetFgain(0, F_gain);
+
+  motorLeft.SetPgain(0, P_gain);
+  motorLeft.SetIgain(0, I_gain);
+  motorLeft.SetDgain(0, D_gain);
+
+  motorRight.SetPgain(0, P_gain);
+  motorRight.SetIgain(0, I_gain);
+  motorRight.SetDgain(0, D_gain); 
+}
+
+void configureMotors()
+{
+  for (byte i=0;i<TOTAL_MOTORS;i++)
+  {
+    motor[i] = CANTalonSRX(i+1,10);
+  }
+  
+  motorRight.SetSensorPhase(true);
+  
+  motorLeft.SetSensorPhase(true);
+  
+  motorLeft.SetMotorInvert(true);
+  motorLeft_Slave1.SetMotorInvert(true);
+  motorLeft_Slave2.SetMotorInvert(true);
+}
+
+void setup(void)
+{
   CANbus0 = FlexCAN(1000000,0,0,0);
   CANbus0.begin();
   
@@ -46,34 +135,13 @@ void setup(void)
   motorLeft.SetParam(CANTalonSRX::eProfileParamSlot_PeakOutput, 150);
   motorRight.SetParam(CANTalonSRX::eProfileParamSlot_PeakOutput, 150);
   
-  motorLeft.SetFgain(0, F_gain);
-  motorLeft_Slave1.SetFgain(0, F_gain);
-  motorLeft_Slave2.SetFgain(0, F_gain);
   
-  motorRight.SetFgain(0, F_gain);
-  motorRight_Slave1.SetFgain(0, F_gain);
-  motorRight_Slave2.SetFgain(0, F_gain);
-
-  motorLeft.SetPgain(0, P_gain);
-  motorLeft.SetIgain(0, I_gain);
-  motorLeft.SetDgain(0, D_gain);
-
-  motorRight.SetPgain(0, P_gain);
-  motorRight.SetIgain(0, I_gain);
-  motorRight.SetDgain(0, D_gain);
+  configurePID();
   
-  motorLeft.GetParamRaw(CANTalonSRX::eProfileParamSlot_F, 0x00,0x00,0x00);
-  motorRight.GetParamRaw(CANTalonSRX::eProfileParamSlot_F, 0x00,0x00,0x00);
-  delay(3000);
+  configureMotors();
 
   
-  motorRight.SetSensorPhase(true);
   
-  motorLeft.SetSensorPhase(true);
-  
-  motorLeft.SetMotorInvert(true);
-  motorLeft_Slave1.SetMotorInvert(true);
-  motorLeft_Slave2.SetMotorInvert(true);
 }
 
 void loop(void)
@@ -94,21 +162,7 @@ void loop(void)
     motorRight_Slave2.update(&msg);
   }
   
-  /*if(CANbus0.available()) 
-  {
-    uint32_t delta = millis() - prev_time;
-    CANbus0.read(msg);
-    Serial.print(millis());
-    Serial.print(" --- ");
-    Serial.print("dt: ");
-    Serial.print(delta);
-    Serial.print(" --- ");
-    printCAN(msg);
-    //Serial.print("CAN bus 0: "); hexDump(8, msg.buf);
-    prev_time = millis();
-  }*/
-  motorLeft.sendMotorEnable(true);
-  motorRight.sendMotorEnable(true);
+  
 
   float left_rev_s = 0.25;
   int left_sp = 2.0 * 1024* left_rev_s / 10;
@@ -126,12 +180,8 @@ void loop(void)
   motorRight_Slave1.SetDemand(CANTalonSRX::kMode_SlaveFollower, RIGHT_MASTER_ID);
   motorRight_Slave2.SetDemand(CANTalonSRX::kMode_SlaveFollower, RIGHT_MASTER_ID);
 
-  
-  //motor1.motorTest();
-  
-  if (millis() - prev_time > 50 && debug == 1)
-  {
-    int32_t right_vel = motorRight.GetSensorVel();
+
+    /*int32_t right_vel = motorRight.GetSensorVel();
     int32_t left_vel = motorLeft.GetSensorVel();
     // counts / s
     // 100 ms
@@ -144,70 +194,8 @@ void loop(void)
     float left_rev_s = (left_vel * 10.0) / 1024.0;
     float left_rad_s = left_rev_s * 3.1415 * 2;
     float left_wheel_rev_s = left_rev_s / 2.0;
-    float left_wheel_rad_s = left_rad_s / 2.0;
+    float left_wheel_rad_s = left_rad_s / 2.0;*/
 
-    Serial.print("RIGHT Sensor Pos: ");
-    Serial.print(motorRight.GetSensorPos());
-    Serial.print(" Enc Pos: ");
-    Serial.print(motorRight.GetEncoderPos());
-    
-    
-    Serial.print(" Sensor Vel: ");
-    Serial.println(right_vel);
-
-    Serial.print("RIGHT Wheel Vel Right: ");
-    Serial.print(right_wheel_rad_s);
-    Serial.print(" rad/s --- ");
-    Serial.print(right_wheel_rev_s);
-    Serial.println(" rev/s");
-
-    Serial.print("RIGHT Target : ");
-    Serial.println(motorRight.getSetPoint());
-    Serial.print("RIGHT Throttle : ");
-    Serial.println(motorRight.GetAppliedThrottle());
-
-    Serial.print("Right Current: ");
-    Serial.print(motorRight.GetCurrent());
-
-    Serial.print(" -- ");
-    Serial.print(motorRight_Slave1.GetCurrent());
-
-    Serial.print(" -- ");
-    Serial.println(motorRight_Slave2.GetCurrent());
-
-
-    Serial.print("LEFT Sensor Pos: ");
-    Serial.print(motorLeft.GetSensorPos());
-    Serial.print(" Enc Pos: ");
-    Serial.print(motorLeft.GetEncoderPos());
-    Serial.print(" Sensor Vel: ");
-    Serial.println(left_vel);
-
-    Serial.print("LEFT Wheel Vel LEFT: ");
-    Serial.print(left_wheel_rad_s);
-    Serial.print(" rad/s --- ");
-    Serial.print(left_wheel_rev_s);
-    Serial.println(" rev/s");
-
-    Serial.print("LEFT Target : ");
-    Serial.println(motorLeft.getSetPoint());
-    Serial.print("LEFT Throttle : ");
-    Serial.println(motorLeft.GetAppliedThrottle());
-
-    Serial.print("Left Current: ");
-    Serial.print(motorLeft.GetCurrent());
-
-    Serial.print(" -- ");
-    Serial.print(motorLeft_Slave1.GetCurrent());
-
-    Serial.print(" -- ");
-    Serial.println(motorLeft_Slave2.GetCurrent());
-    
-
-    motorLeft.printParams();
-    motorRight.printParams();
-    Serial.println("--------------------------------------");
-    prev_time = millis();
-  }
-  delay(1);
+  checkTimers();
+  //nh.spinOnce();
 }
